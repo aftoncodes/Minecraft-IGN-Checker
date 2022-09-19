d9474eaa60f685f0dd07a7e18397b6ed1462930b
666bits <107783820+crypt3rking@users.noreply.github.com>
2022-09-19 22:08:27 +0300

    Add files via upload
    Main program ^^

---
+++ minecraft-ign-checker.py
@@ -0,0 +1,27 @@
+import random
+import string
+import requests
+import time
+
+print("Hello to my Minecraft IGN generator and checker!\nMade with love by 666bits <3")
+N = 3
+time.sleep(1)
+tries = int(input("How many IGNs would you like to generate: "))
+for i in range(tries):
+    ran_str = ''.join(random.choices(string.ascii_lowercase + string.digits + '_', k=N))
+    url = f'https://api.mojang.com/users/profiles/minecraft/{ran_str}?'
+    response = requests.get(url)
+    uuid = response.json()['id']
+    if uuid is not None:
+        print(ran_str, 'is invalid.')
+        tries = tries - 1
+        if tries == 0:
+            if uuid is not None:
+                print('Sorry, no valid IGN found :(')
+                time.sleep(10)
+        elif tries != 0:
+            tries = tries
+    elif uuid is None:
+        print("Valid IGN found!", ran_str)
+        break
+        time.sleep(10)
